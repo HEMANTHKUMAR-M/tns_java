62433d15dfd03380077964a61bdf425134532873
hemanth kumar <hemantha1996m@gmail.com>
2026-02-12 10:43:03 +0530

    first commit

--- svit1/src/Basics/HelloWorld.java
+++ svit1/src/Basics/HelloWorld.java
@@ -1,11 +1,18 @@
 package Basics;
 
 public class HelloWorld {
+	
+	public static void training(){
+		System.out.println("Iam doing  Java Training in TNS Foundation");
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello");
+		training();
+		
 
 	}
 
---
+++ svit1/src/org/tns/java/training/Demo.java
@@ -0,0 +1,19 @@
+package org.tns.java.training;
+
+public class Demo {
+	
+	public static void training() {
+		System.out.println(" iam very much intrested in java Progranmming");
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		training();
+		
+
+	}
+
+}
